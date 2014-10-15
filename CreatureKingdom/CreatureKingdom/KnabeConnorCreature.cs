996f6a8be44c025f0edae17e655d802519573e45
Connor Knabe <ConnorKnabe@gmail.com>
2014-10-15 10:03:05 -0500

    Seems to be inheriting properly

--- CreatureKingdom/CreatureKingdom/KnabeConnorCreature.cs
+++ CreatureKingdom/CreatureKingdom/KnabeConnorCreature.cs
@@ -4,7 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+using System.Windows.Threading;
+
 namespace CreatureKingdom {
-    class KnabeConnorCreature {
+    class KnabeConnorCreature :Creature {
+        
+        public KnabeConnorCreature(Canvas kingdom, Dispatcher dispatcher, Int32 waitTime)
+            : base(kingdom, dispatcher, waitTime) {
+        }
+
+        public override void Shutdown(){
+
+
+        }
+
     }
 }
